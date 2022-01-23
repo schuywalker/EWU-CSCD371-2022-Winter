7200e0203f285399cb4b2c128a199906731d1554
schuywalker <sasplin@ewu.edu>
2022-01-23 12:08:22 -0800

    add Whiteny's changes

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,41 @@
+﻿namespace Logger
+{
+    // Create a FileLogger that derives from BaseLogger.
+    // It should take in a path to a file to write the log message to.When its Log method is called,
+    // it should append messages on their own line in the file.The output should include all of the following:
+    // The current date/time ❌✔ The name of the class that created the logger ❌✔ The log level ❌✔ The message ❌✔
+    // The format may vary, but an example might look like this
+    // "10/7/2019 12:38:59 AM FileLoggerTests Warning: Test message"
+
+    //References for research
+    //https://stackoverflow.com/questions/20185015/how-to-write-log-file-in-c
+    //https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-open-and-append-to-a-log-file
+
+    public class FileLogger : BaseLogger
+    {
+        public FileLogger()
+        {
+        }
+
+        public override void Log(LogLevel logLevel, string message)
+        {
+            DateTime dateTime = DateTime.Now;
+
+            // * Code Reference: https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-open-and-append-to-a-log-file
+            using (StreamWriter w = File.AppendText("file.txt"))
+            {
+                w.Write(dateTime);
+                w.Write(Environment.NewLine);
+                w.Write("LogFactory");
+                w.Write(Environment.NewLine);
+                w.Write(logLevel);
+                w.Write(Environment.NewLine);
+                w.Write(message);
+                w.Write(Environment.NewLine);
+            }
+
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,11 +1,23 @@
 ﻿namespace Logger
 {
+    // References: CSCD371 Lecture
     public class LogFactory
     {
+
         public BaseLogger CreateLogger(string className)
         {
-            
             return null;
         }
+
+        // There is an existing BaseLogger class. It needs an auto property to hold the class name.
+        // This property should be set in the LogFactory using an object initializer.
+        new BaseLogger className { get; set; }
+        //References for research
+        //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/how-to-initialize-objects-by-using-an-object-initializer
+        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/auto-implemented-properties
+
+        //The LogFactory should be updated with a new method ConfigureFileLogger.
+        //This should take in a file path and store it in a private member.
+        //It should use this when instantiating a new FileLogger in its CreateLogger method. ❌✔
     }
 }
