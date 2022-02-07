db5aa3d1d13fdfee1d86bd6c30b47f2cc7eda62f
schuywalker <sasplin@ewu.edu>
2022-02-06 18:27:40 -0800

    add T getter and ReturnType() in CLL

--- GenericsHomework/GenericsHomework/CircularlyLinkedList.cs
+++ GenericsHomework/GenericsHomework/CircularlyLinkedList.cs
@@ -3,18 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GenericsHomework;
 
-    public class CircularlyLinkedList
-    {
-        public CircularlyLinkedList()
+    public class CircularlyLinkedList<T>
+
+       
     {
 
+    //public int Size { get; private set; } = 0;
+    public Node<T>? Cursor { get; set; }
+    public Node<T>? Head { get; set; }
+    public int Size { get; private set; } = 0;
+    
+    public T this[int index]
+    {
+        get => ReturnType(index)!; // if null, exception is thrown  
     }
 
-    public void Append()
+
+
+
+    
+
+    public void Append(Node<T> newNode)
     {
+        
+
+        //Size++;
+
 
     }
 
@@ -22,12 +40,78 @@ namespace GenericsHomework;
     {
 
     }
-    public bool Exists()
+    public bool Exists(int index)
     {
         return false;
     }
+    public T? ReturnType(int index)
+    {
+        Node<T> CursorTemp = Cursor!;//about to be null checked
+        if (Cursor is null || Head is null) throw new InvalidOperationException();
+        else if (index == Cursor.Index)
+        {
+            return Cursor.NodeType;
+        }
+        
+        else if (CursorTemp.Index < index)
+        {
+
+            while (CursorTemp.Index < index)
+            {
+                CursorTemp = CursorTemp.Next;
+            }
+            return CursorTemp.NodeType;
+        }
+        else
+        {
+            CursorTemp = Head;
+            while (CursorTemp.Index < index)
+            {
+                CursorTemp = CursorTemp.Next;
+            }
+            return CursorTemp.NodeType;
+        }
+    }
 
 
+    public class Node<TNodeType>
+    //where T : notnull, new()
+
+    {
+        
+        public Node<TNodeType> Next { get; private set; }
+        public int Index { get; set; }
+
+        public Node(T t, Node<TNodeType>? next, int index)
+        {
+            if (next is null)
+            {
+                Next = this;
+            }
+            else
+            {
+                Next = next;
+            }
+            NodeType = t;
+        }
+
+        //public void SetNext(Node<TNodeType> next)
+        //{
+        //    Next = next;
+        //}
+        
+        [DisallowNull]
+        public T NodeType { get; set; }
+        
+
+
+        public override string ToString()
+        {
+            return "Node of type " + typeof(T); //+ " with index: " + Index;
+        }
 
     }
+
+
+}
 
