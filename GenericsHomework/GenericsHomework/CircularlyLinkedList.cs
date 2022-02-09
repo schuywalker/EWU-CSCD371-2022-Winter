ba17b74071fd09112e54ce84a92720fc6c86d277
schuywalker <sasplin@ewu.edu>
2022-02-08 22:36:41 -0800

    Commit before moving methods to node class. I'm resistant to it from a SOLID perspective but it seems like the best solution given that set is required to be private

--- GenericsHomework/GenericsHomework/CircularlyLinkedList.cs
+++ GenericsHomework/GenericsHomework/CircularlyLinkedList.cs
@@ -14,128 +14,82 @@ public class CircularlyLinkedList<T>
 
     public Node<T>? Tail { get; set; }
     public Node<T>? Head { get; set; }
-    public int Size { get; private set; } = 0;
-
-    public T this[int index]
-    {
-
-        get
-        {
-            if (Tail is null || Head is null) throw new InvalidOperationException();
-            else
-            {
-                return ReturnType(index)!; // if null, exception is thrown  
-            }
-
-        }
-    }
+    
 
 
 
 
-    public void Append([DisallowNull] T t) // with no set next, append will need node to set next in constructor, and append will
-        //need to make new(). therefore append must be in the Node class.
+    public void Append([DisallowNull] T t)
     {
         if (Head is null || Tail is null)
         {
 
-            Node<T> newNode = new(t, 0);
+            Node<T> newNode = new(t);
             Head = newNode;
             Tail = newNode;
         }
         else
         {
-
-            Node<T> temp = new(t, Tail, Tail.Index + 1);
-            
+            Node<T> temp = new(t, Tail);
         }
-        Size++;
+
     }
 
-    public void Clear()
+    public void Clear(Node<T> node)
     {
-
+        if (node is null) throw new ArgumentNullException("Argument cannot be null");
+        if (Head == node && Tail == node) return; // CLL size == 1: do nothing
+        else 
+        {
+            Head = null;
+            Tail = null;
+            Type type = typeof(T) ?? throw new InvalidDataException("Type of node cannot be null");
+            Append(type);
+        }
+        // size > 1, head == node
+        //size > 1, tail == node
+        // size > 1, node in middle of list
     }
-    public bool Exists(int index)
+    public bool Exists(T Value)
     {
         return false;
-    }
-    public T ReturnType(int index)
-    {
-        
-        Node<T> CursorTemp = Tail!;//about to be null checked
-        if (Tail is null || Head is null) throw new InvalidOperationException();
-        else if (index == Tail.Index)
-        {
-            return Tail.NodeType;
-        }
-
-        else if (CursorTemp.Index < index)
-        {
-
-            while (CursorTemp.Index < index)
-            {
-                CursorTemp = CursorTemp.Next;
-            }
-            return CursorTemp.NodeType;
-        }
-        else
-        {
-            CursorTemp = Head;
-            while (CursorTemp.Index < index)
-            {
-                CursorTemp = CursorTemp.Next;
-            }
-            return CursorTemp.NodeType;
-        }
-    }
-
-
-
-    public class Node<TNodeData>
-    where TNodeData : notnull
-
-    {
-
-        [DisallowNull]
-        public Node<TNodeData> Next { get; private set; }
-        public int Index { get; set; }
-        
-        [DisallowNull]
-        public TNodeData NodeType { get; set; }
-
-        public Node([DisallowNull] TNodeData t, int index)
-        { // type of Node is always T, but once it's in node, we call it TNodeData
-
-            NodeType = t;
-            Next = this;
-            Index = index;
-        }
-        
-
-        public Node([DisallowNull] TNodeData t, Node<TNodeData> Tail, int index)
-        { // type of Node is always T, but once it's in node, we call it TNodeData
-
-            NodeType = t;
-            Index = index;
-
-            Next = Tail.Next;// AKA Head
-            Tail.Next = this;
-            Tail = this;
-
-           
-        }
-
-
-        public override string ToString()
-        {
-            return "Node of type " + typeof(T) + " with index: " + Index;
-        }
-
     }
 
 
 }
+    //public T ReturnType(int index)
+    //{
+        
+    //    Node<T> CursorTemp = Tail!;//about to be null checked
+    //    if (Tail is null || Head is null) throw new InvalidOperationException();
+    //    else if (index == Tail.Index)
+    //    {
+    //        return Tail.NodeType;
+    //    }
+
+    //    else if (CursorTemp.Index < index)
+    //    {
+
+    //        while (CursorTemp.Index < index)
+    //        {
+    //            CursorTemp = CursorTemp.Next;
+    //        }
+    //        return CursorTemp.NodeType;
+    //    }
+    //    else
+    //    {
+    //        CursorTemp = Head;
+    //        while (CursorTemp.Index < index)
+    //        {
+    //            CursorTemp = CursorTemp.Next;
+    //        }
+    //        return CursorTemp.NodeType;
+    //    }
+    //}
+
+
+
+
 
 // interesting interview question:
 // im writing method, you pass me node, return whether the node in question eventually ends in null or circles back on itself
---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHomework;
+public class Node<T>
+where T : notnull
+
+{
+
+    [DisallowNull]
+    public Node<T> Next { get; private set; }
+    public int Index { get; set; }
+
+    [DisallowNull]
+    public T NodeType { get; set; }
+
+    public Node([DisallowNull] T t)
+    { 
+
+        NodeType = t;
+        Next = this;
+
+    }
+
+
+    public Node([DisallowNull] T t, [DisallowNull] Node<T> Tail)
+    { 
+        NodeType = t;
+
+        Next = Tail.Next;// AKA Head
+        Tail.Next = this;
+        Tail = this;
+
+    }
+    public Node([DisallowNull] Node<T> node)
+    {
+        node.Next = node;
+    }
+
+
+    public override string ToString()
+    {
+        return "Node of type " + typeof(T) + " with index: " + Index;
+    }
+
+}
+
+
+
