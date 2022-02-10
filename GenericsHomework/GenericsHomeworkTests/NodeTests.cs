3cc8c759edc95b6ef6e76a09993cd23ab6fb36f7
schuywalker <sasplin@ewu.edu>
2022-02-09 16:04:39 -0800

    Add Node() Clear() Append() Unit tests

--- GenericsHomework/GenericsHomeworkTests/NodeTests.cs
+++ GenericsHomework/GenericsHomeworkTests/NodeTests.cs
@@ -4,22 +4,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GenericsHomework.Tests
 {
     [TestClass]
     public class MyTestClass
     {
-        
+
+        //[TestMethod]
+        //[ExpectedException()]
+        //public void Node_NullArg_Exception()
+        //{
+        //    Node<string> test = new(null!);
+        //}
+
+
         [TestMethod]
-        public void Node_DataTypeAsExcepted_Success()
+        public void Node_SingleNodePointsToSelf_Success()
         {
-            
+            Node<string> testNode = new("test string");
+            Assert.AreEqual<string>("test string", testNode.Next.Value);
         }
-        public void NodeConstructor_NextSetToSelf_Success()
+        [TestMethod]
+        public void Append_ListIsCircular_Success()
         {
+            Node<string> testNode = new("first node string value");
+            testNode.Append("2nd node 42");
+            testNode.Next.Append("3 is magic");
+
+            Node<string> Cursor = testNode.Next;
+            Assert.AreEqual<string>("2nd node 42", Cursor.Value);
+
+            Cursor = Cursor.Next;
+            Assert.AreEqual<string>("3 is magic", Cursor.Value);
+
+            Cursor = Cursor.Next;
+            Assert.AreEqual<string>("first node string value", Cursor.Value);
+
+            Assert.IsTrue(Cursor == testNode);
 
         }
+        [TestMethod]
+        public void Clear_WorksOnFirstNode_Success()
+        {
+            Node<string> testNode = new("first node string value");
+            testNode.Append("2nd node 42");
+            testNode.Next.Append("3 is magic");
+
+            testNode.Clear();
+            Assert.AreEqual<string>("first node string value", testNode.Value);
+            Assert.AreEqual<string>("first node string value", testNode.Next.Value);
+        }
     }
-    
+
 }
