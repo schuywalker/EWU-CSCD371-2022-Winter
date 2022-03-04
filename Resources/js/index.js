a388d52c08c305328283bc659fe26987e53dcd1e
A-Tanner <atanner5@ewu.edu>
2022-03-04 12:13:14 -0800

    Buttons Working

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -1,18 +1,18 @@
-document.getElementsByClassName("jokeButton")[0].addEventListener("click", getJoke);
+document.getElementsByClassName("joke-button")[0].addEventListener("click", getJoke);
 
 function getJoke() {
     axios.get("https://v2.jokeapi.dev/joke/Programming").then((response) => {
         console.log(response);
         if (response.data.type == "single") {
-            document.getElementsByClassName("jokeLine1")[0].innerHTML = response.data.joke;
-            document.getElementsByClassName("jokeLine2")[0].innerHTML = "";
+            document.getElementsByClassName("joke-line1")[0].innerHTML = response.data.joke;
+            document.getElementsByClassName("joke-line2")[0].innerHTML = "";
         } else {
-            document.getElementsByClassName("jokeLine1")[0].innerHTML = response.data.setup;
+            document.getElementsByClassName("joke-line1")[0].innerHTML = response.data.setup;
             setTimeout(delayPunchline, 4000, response.data.delivery);
         }
     });
 }
 
 function delayPunchline(joke) {
-    document.getElementsByClassName("jokeLine2")[0].innerHTML = joke;
+    document.getElementsByClassName("joke-line2")[0].innerHTML = joke;
 }
