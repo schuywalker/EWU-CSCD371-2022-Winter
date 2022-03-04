f5e78e1e6a732cc58d834e93d12e0ad036e6b59c
schuyler <sasplin@ewu.edu>
2022-03-04 11:39:45 -0800

    change table to joke-card and add header, body, footer inside card

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -4,15 +4,15 @@ function getJoke() {
     axios.get("https://v2.jokeapi.dev/joke/Programming").then((response) => {
         console.log(response);
         if (response.data.type == "single") {
-            document.getElementsByClassName("jokeLine1")[0].innerHTML = (response.data.joke);
+            document.getElementsByClassName("jokeLine1")[0].innerHTML = response.data.joke;
             document.getElementsByClassName("jokeLine2")[0].innerHTML = "";
         } else {
-            document.getElementsByClassName("jokeLine1")[0].innerHTML = (response.data.setup);
-            setTimeout(delayPunchline, 4000,response.data.delivery);
+            document.getElementsByClassName("jokeLine1")[0].innerHTML = response.data.setup;
+            setTimeout(delayPunchline, 4000, response.data.delivery);
         }
     });
 }
 
-function delayPunchline(joke){
+function delayPunchline(joke) {
     document.getElementsByClassName("jokeLine2")[0].innerHTML = joke;
 }
