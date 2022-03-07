33bce93a61a2487d43ef56766402ddc39487b9f3
A-Tanner <atanner5@ewu.edu>
2022-03-07 12:30:48 -0800

    Menu hidden, displays on button press

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -1,5 +1,7 @@
 document.getElementsByClassName("joke-button")[0].addEventListener("click", getJoke);
 
+var menuDisplayed = false;
+
 function getJoke() {
     axios.get("https://v2.jokeapi.dev/joke/Programming").then((response) => {
         console.log(response);
@@ -10,7 +12,10 @@ function getJoke() {
             document.getElementsByClassName("joke-line1")[0].innerHTML = response.data.setup;
             setTimeout(delayPunchline, 4000, response.data.delivery);
         }
-    });
+    }).catch(function (error) {
+    document.getElementsByClassName("joke-line1")[0].innerHTML = "Joke Service is down, try again in a few moments.";
+    document.getElementsByClassName("joke-line2")[0].innerHTML = "";
+})
 }
 
 function delayPunchline(joke) {
@@ -18,5 +23,15 @@ function delayPunchline(joke) {
 }
 
 function revealMenu() {
-    console.log("menuuuuuuu");
+
+    menuDisplayed = !menuDisplayed
+
+    if (menuDisplayed){
+        document.getElementsByClassName("dropdown-menu")[0].style.display = "block";
+    }
+    else{
+        document.getElementsByClassName("dropdown-menu")[0].style.display = "none";
+    }
+    
+        
 }
