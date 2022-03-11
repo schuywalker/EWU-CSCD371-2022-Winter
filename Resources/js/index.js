b1beda6b9cafe018db3db0cb7a20d2a79d3c7e71
schuyler <sasplin@ewu.edu>
2022-03-10 23:57:45 -0800

    win-big button

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -1,23 +1,29 @@
 document.getElementsByClassName("joke-button")[0].addEventListener("click", getJoke);
+document.getElementsByClassName("money-button")[0].addEventListener("click", winBigBaby);
 
 var menuDisplayed = false;
 var flipped = false;
+const winning = false;
 
 function getJoke() {
-    axios.get("https://v2.jokeapi.dev/joke/Programming").then((response) => {
-        console.log(response);
-        if (response.data.type == "single") {
-            document.getElementsByClassName("joke-line1")[0].innerHTML = response.data.joke;
+    axios
+        .get("https://v2.jokeapi.dev/joke/Programming")
+        .then((response) => {
+            console.log(response);
+            if (response.data.type == "single") {
+                document.getElementsByClassName("joke-line1")[0].innerHTML = response.data.joke;
+                document.getElementsByClassName("joke-line2")[0].innerHTML = "";
+            } else {
+                document.getElementsByClassName("joke-line1")[0].innerHTML = response.data.setup;
+                document.getElementsByClassName("joke-line2")[0].innerHTML = "";
+                setTimeout(delayPunchline, 4000, response.data.delivery);
+            }
+        })
+        .catch(function (error) {
+            document.getElementsByClassName("joke-line1")[0].innerHTML =
+                "Joke Service is down, try again in a few moments.";
             document.getElementsByClassName("joke-line2")[0].innerHTML = "";
-        } else {
-            document.getElementsByClassName("joke-line1")[0].innerHTML = response.data.setup;
-            document.getElementsByClassName("joke-line2")[0].innerHTML = "";
-            setTimeout(delayPunchline, 4000, response.data.delivery);
-        }
-    }).catch(function (error) {
-    document.getElementsByClassName("joke-line1")[0].innerHTML = "Joke Service is down, try again in a few moments.";
-    document.getElementsByClassName("joke-line2")[0].innerHTML = "";
-})
+        });
 }
 
 function delayPunchline(joke) {
@@ -25,24 +31,33 @@ function delayPunchline(joke) {
 }
 
 function revealMenu() {
+    menuDisplayed = !menuDisplayed;
 
-    menuDisplayed = !menuDisplayed
-
-    if (menuDisplayed){
+    if (menuDisplayed) {
         document.getElementsByClassName("dropdown-menu")[0].style.display = "block";
-    }
-    else{
+    } else {
         document.getElementsByClassName("dropdown-menu")[0].style.display = "none";
     }
-    
-        
 }
 
-function flipBody(){
+function flipBody() {
     flipped = !flipped;
-    if (flipped){
-    document.getElementsByTagName("body")[0].classList.add("invert")
+    if (flipped) {
+        document.getElementsByTagName("body")[0].classList.add("invert");
     } else {
         document.getElementsByTagName("body")[0].classList.remove("invert");
+    }
+}
+
+function winBigBaby() {
+    winning = !winning;
+    if (winning) {
+        document
+            .getElementsByTagName("body")[0]
+            .classList.add("background-image: ./piles-of-big-money.jpeg");
+    } else {
+        document
+            .getElementsByTagName("body")[0]
+            .classList.remove("background-image: ./piles-of-big-money.jpeg");
     }
 }
